10f2921b412d7c058c131b2ac737069c2294ec63
Crystal Tate <crys.tate@icloud.com>
2017-04-02 18:24:08 -0500

    Create User

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,27 @@ var express = require("express");
 	router = express.Router();
 	knex = require("../db/knex")
 
-router.route("/");
+
+// for '/users' handling (this is where .post, .delete & .put will be included)
+router.route('/')
+	.post((req, res) => {
+		knex('users')
+			.insert({
+				full_name: req.body.user.full_name,
+				username: req.body.user.username,
+				img_url: req.body.user.img_url
+			})
+			.returning('id')
+			.then(function(id) {
+				res.redirect(`/users/${id}`);
+			});
+	});
+
+
+// this is where users will go to add a new user
+router.route("/new")
+	.get(function(req, res){
+	  res.render('users/new');
+	});
 
 module.exports = router;
